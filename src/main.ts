ee64286b04d3b4edea07c338addbd027d7d7465c
Pedro Elias <elias.pedrocontato@gmail.com>
2025-07-07 16:55:02 -0300

    testes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal - Pedro Elias')
+    .setDescription(
+      'Projeto Blog Pessoal desenvolvido com NestJs durante o bootcamp JavaScript da Generation Brasil.',
+    )
+    .setContact(
+      'Pedro Elias',
+      'https://github.com/pedro-eliasd',
+      'elias.pedrocontato@gmail.com',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '03:00';
 
--- src/tema/controllers/tema.controller.ts
+++ src/tema/controllers/tema.controller.ts
@@ -11,12 +11,15 @@ import {
   Put,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 import { Tema } from '../entities/tema.entity';
 import { TemaService } from '../services/tema.service';
 
+@ApiTags('Tema')
 @UseGuards(JwtAuthGuard)
 @Controller('/temas')
+@ApiBearerAuth()
 export class TemaController {
   constructor(private readonly temaService: TemaService) {}
 
